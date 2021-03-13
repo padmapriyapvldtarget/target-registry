bd89ba9cf1b9bdc77e6bee739c2bdce1eeaecac5
padmapriya <padmapriya@padmapriyas-MacBook-Pro.local>
2021-03-13 10:51:46 +0530

    Target Registry Changes

---
+++ src/main/java/com/target/registry/TargetRegistryApplication.java
@@ -0,0 +1,16 @@
+ package com.target.registry;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TargetRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargetRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/target/registry/TargetRegistryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.target.registry;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
